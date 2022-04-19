f582d42d34a89525c2414f2b20e33901459224a5
jugal kishor singh <jugals@mindfiresolutions.com>
2022-04-19 13:35:20 +0530

    back-end - microservice refactor

---
+++ back-end/employment-service/src/test/java/com/cms/employmentservice/EmploymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cms.employmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmploymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
